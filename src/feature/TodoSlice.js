c070afe3586c464a4f78fb1afad511503d14440f
Anees <aneesaboo123@gmail.com>
2025-07-12 06:17:55 +0530

    fixed:todoSlice

---
+++ src/feature/TodoSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { compile } from "tailwindcss";
+
+const TodoSlice = createSlice({
+  name: "todo",
+  initialState: [],
+
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        todoText: action.payload,
+        todoId: Date.now(),
+        complete: false,
+      };
+      state.push(newTodo);
+    },
+    editTodo: (state, action) => {
+      const { todoText, todoId } = action.payload;
+
+      state.map((todo) => {
+        if (todo.todoId === todoId) {
+          todo.todoText === todoText;
+        }
+        return todo;
+      });
+    },
+    toggleTodo: (state, action) => {
+      state.map((todo) => {
+        if (todo.todoId === action.payload) {
+          todo.complete = !todo.complete;
+        }
+        return todo;
+      });
+    },
+    deleteTodo: (state, action) => {
+      state.map((todo) => {
+        return todo.todoId != action.payload;
+      });
+    },
+  },
+});
+
+export const { editTodo, deleteTodo, addTodo } = TodoSlice.actions;
+export default TodoSlice.reducer;
